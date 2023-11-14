bf9c6f9acc7218b2faac1d53e0a371e769960eef
vyadir <yadir.sve@gmail.com>
2023-11-13 18:06:01 -0600

    característica de restricciones agregada

--- app.py
+++ app.py
@@ -13,9 +13,63 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from dotenv import load_dotenv
-
+from abc import ABC, abstractmethod
+import random
 # Cargar las variables de entorno desde .env
 load_dotenv()
+
+# Estrategia Base
+class SorteoEstrategia(ABC):
+    @abstractmethod
+    def asignar_amigos(self, participantes, restricciones):
+        pass
+
+# Estrategia Concreta A
+class SorteoSimpleEstrategia(SorteoEstrategia):
+    def asignar_amigos(self, participantes, restricciones):
+        amigos = list(participantes.keys())
+        random.shuffle(amigos)
+        return dict(zip(amigos, amigos))
+
+class SorteoComplejoEstrategia(SorteoEstrategia):
+    def asignar_amigos(self, participantes, restricciones):
+        # Crear una copia de los participantes para trabajar con ella
+        amigos = list(participantes.keys())
+        random.shuffle(amigos)
+
+        asignaciones = {}
+        intentos = 0
+        max_intentos = 1000
+
+        while len(asignaciones) < len(amigos):
+            for donante in amigos:
+                posibles_receptores = [r for r in amigos if r != donante and r not in asignaciones.values() and r not in restricciones.get(donante, [])]
+                
+                if not posibles_receptores:
+                    # Si no hay posibles receptores, reiniciar el proceso
+                    asignaciones.clear()
+                    random.shuffle(amigos)
+                    intentos += 1
+                    break
+                
+                receptor = random.choice(posibles_receptores)
+                asignaciones[donante] = receptor
+
+            if intentos > max_intentos:
+                raise Exception("No se puede generar una asignación válida con las restricciones dadas.")
+
+        return asignaciones
+
+
+class AmigoSecreto:
+    def __init__(self, estrategia: SorteoEstrategia, participantes, restricciones=None):
+        self.estrategia = estrategia
+        self.participantes = participantes
+        self.restricciones = restricciones or {}
+
+    def asignar_amigos(self):
+        return self.estrategia.asignar_amigos(self.participantes, self.restricciones)
+
 
 class GestorCorreo:
     """Clase para gestionar la conexión SMTP y enviar correos electrónicos."""
@@ -45,7 +99,7 @@ class GestorCorreo:
         # Crear un mensaje de correo electrónico con un asunto, remitente y destinatario.
         mensaje = MIMEMultipart()
         mensaje['Subject'] = asunto
-        mensaje['From'] = self.usuario
+        mensaje['From'] = "Amigo Secreto 2023"
         mensaje['To'] = destinatario
 
         # Adjuntar el cuerpo del correo como HTML para poder incluir imágenes y formateo.
@@ -78,19 +132,63 @@ class GestorCorreo:
 
 class AmigoSecreto:
     """Clase para manejar la lógica del sorteo del amigo secreto."""
-    def __init__(self, participantes):
+
+    def __init__(self, participantes, restricciones=None):
         # Inicializar con un diccionario de participantes y sus correos electrónicos.
+        # Añadir un diccionario de restricciones donde la clave es quien no puede regalar a quien.
         self.participantes = participantes
+        self.restricciones = restricciones or {}
+
+    def verificar_restricciones(self, asignaciones):
+        # Verificar si las asignaciones actuales violan alguna restricción.
+        for donante, receptor in asignaciones.items():
+            if receptor in self.restricciones.get(donante, []):
+                return False
+        return True
+
+    def detectar_ciclos(self):
+        # Función para detectar ciclos en las restricciones.
+        visitado = set()
+        
+        def visitar(nodo, camino):
+            if nodo in camino:
+                return True  # Ciclo detectado.
+            camino.add(nodo)
+            for vecino in self.restricciones.get(nodo, []):
+                if visitar(vecino, camino):
+                    return True
+            camino.remove(nodo)
+            return False
+        
+        for nodo in self.restricciones:
+            if visitar(nodo, set()):
+                return True  # Hay un ciclo.
+        return False  # No hay ciclos.
 
     def asignar_amigos(self):
         # Asignar amigos secretos de manera aleatoria.
         amigos = list(self.participantes.keys())
         random.shuffle(amigos)
-        # Asegurarse de que nadie se asigne a sí mismo.
-        while any(nombre == amigo for nombre, amigo in zip(self.participantes, amigos)):
+        asignaciones = dict(zip(amigos, amigos))
+
+        # Asegurarse de que las asignaciones cumplan con las restricciones.
+        while not self.verificar_restricciones(asignaciones):
             random.shuffle(amigos)
-        # Devolver un diccionario con las asignaciones.
-        return dict(zip(self.participantes, amigos))
+            asignaciones = dict(zip(amigos, amigos))
+
+        # Verificar que nadie se auto-asigne y que se respeten las restricciones.
+        intentos = 0
+        max_intentos = 1000
+        while any(nombre == amigo for nombre, amigo in asignaciones.items()) or not self.verificar_restricciones(asignaciones):
+            if intentos > max_intentos:
+                raise Exception("No se puede generar una asignación válida con las restricciones dadas.")
+            random.shuffle(amigos)
+            asignaciones = dict(zip(amigos, amigos))
+            intentos += 1
+
+        # Devolver el diccionario con las asignaciones.
+        return asignaciones
+
 
 # Interfaz gráfica de usuario
 class Aplicacion:
@@ -125,6 +223,20 @@ class Aplicacion:
         # Frame para el formulario de entrada
         self.frame_formulario = Frame(master, bg=color_fondo)
         self.frame_formulario.pack(pady=10)
+
+        # Agregar interfaz para restricciones
+        self.restricciones = {}  # Diccionario para almacenar restricciones
+        self.restricciones_var = StringVar(master)
+
+        # Etiqueta y campo de entrada para las restricciones
+        self.etiqueta_restricciones = Label(self.frame_formulario, text="Restricciones (formato: Luis no Juan)", font=fuente_texto, bg=color_fondo, fg=color_texto)
+        self.etiqueta_restricciones.grid(row=3, column=0, padx=10, sticky=W)
+        self.entrada_restricciones = Entry(self.frame_formulario, textvariable=self.restricciones_var, font=fuente_texto, width=30)
+        self.entrada_restricciones.grid(row=3, column=1, padx=10, pady=10)
+
+        # Botón para agregar restricciones
+        self.boton_agregar_restriccion = Button(self.frame_formulario, text="Agregar Restricción", font=fuente_texto, bg=color_botones, fg=color_acento, command=self.agregar_restriccion)
+        self.boton_agregar_restriccion.grid(row=4, column=0, columnspan=2, pady=10)
 
         # Etiqueta y campo de entrada para el nombre
         self.etiqueta_nombre = Label(self.frame_formulario, text="Nombre", font=fuente_texto, bg=color_fondo, fg=color_texto)
@@ -173,39 +285,65 @@ class Aplicacion:
         else:
             print("Nombre o correo no puede estar vacío")
 
+    def agregar_restriccion(self):
+        # Obtener la restricción del campo de entrada y actualizar el diccionario de restricciones
+        restriccion = self.restricciones_var.get()
+        if restriccion:
+            donante, _, receptor = restriccion.partition(' no ')
+            if donante and receptor:
+                if donante in self.restricciones:
+                    self.restricciones[donante].append(receptor)
+                else:
+                    self.restricciones[donante] = [receptor]
+                self.texto_participantes.insert(END, f"Restricción agregada: {donante} no puede regalar a {receptor}\n")
+                self.restricciones_var.set('')
+            else:
+                messagebox.showwarning("Advertencia", "Formato de restricción incorrecto")
+        else:
+            messagebox.showwarning("Advertencia", "La restricción no puede estar vacía")
+
+    
     def realizar_sorteo_enviar_correos(self):
-        juego_amigo_secreto = AmigoSecreto(self.participantes)
-        parejas = juego_amigo_secreto.asignar_amigos()
-        gestor_correo = GestorCorreo()
-        ruta_imagen = 'Sorteo_Amigo_Secreto/Img/regalo.jpg'
-        mensajes_exitosos = []  # Lista para almacenar mensajes de confirmación
-        for persona, amigo in parejas.items():
-            asunto = "Tu amigo secreto 2023, Familia Vega Espinoza"
-            cuerpo = f"""
-                        <html>
-                            <body>
-                                <p>Hola, espero que estés de maravilla {persona},</p>
-                                <p>Tu amigo secreto para el regalo es: {amigo}</p>
-                                <p><img src="cid:{os.path.basename(ruta_imagen)}"></p>
-                            </body>
-                        </html>
-                        """
+        # Seleccionar la estrategia basada en si hay restricciones definidas o no.
+        estrategia = SorteoComplejoEstrategia() if self.restricciones else SorteoSimpleEstrategia()
+        juego_amigo_secreto = AmigoSecreto(estrategia, self.participantes, self.restricciones)
+        
+        try:
+            parejas = juego_amigo_secreto.asignar_amigos()
+            gestor_correo = GestorCorreo()
+            ruta_imagen = 'Sorteo_Amigo_Secreto/Img/regalo.jpg'
+            mensajes_exitosos = []
+            for persona, amigo in parejas.items():
+                asunto = "Tu amigo secreto 2023"
+                cuerpo = f"""
+                            <html>
+                                <body>
+                                    <p>Hola, espero que estés de maravilla {persona},</p>
+                                    <p>Tu amigo secreto es: {amigo}</p>
+                                    <p><img src="cid:{os.path.basename(ruta_imagen)}"></p>
+                                    <p>¡Saludos!</p>
+                                </body>
+                            </html>
+                            """
             try:
                 gestor_correo.enviar_correo(self.participantes[persona], asunto, cuerpo, ruta_imagen)
                 mensajes_exitosos.append(f"Correo enviado con éxito a {persona}")
             except Exception as e:
                 messagebox.showerror("Error", f"No se pudo enviar el correo a {persona}: {e}")
-                
-        gestor_correo.cerrar_conexion()
-        # Mostrar un mensaje pop-up con los resultados del envío
-        if mensajes_exitosos:  # Si hay mensajes exitosos para mostrar
-            messagebox.showinfo("Correos Enviados", "\n".join(mensajes_exitosos) + "\nSorteo realizado y correos enviados.")
+            gestor_correo.cerrar_conexion()
+            if mensajes_exitosos:
+                messagebox.showinfo("Correos Enviados", "\n".join(mensajes_exitosos) + "\nSorteo realizado y correos enviados.")
+        except Exception as e:
+            messagebox.showerror("Error", f"No se pudo realizar el sorteo: {e}")
 
-
-
-# Ejemplo de uso
 if __name__ == '__main__':
     root = Tk()
     app = Aplicacion(root)
+    # Aquí debes definir las restricciones, por ejemplo:
+    restricciones = {
+        'Luis': ['Juan'],  # Luis no puede regalar a Juan
+        # Puedes agregar más restricciones aquí
+    }
+    juego_amigo_secreto = AmigoSecreto(app.participantes, restricciones)
     root.mainloop()
 
